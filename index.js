b1e859deb247c0af99173d3a1ef98ffcca15344b
Alka sah <alkasah485@gmail.com>
2022-08-16 01:58:23 -0700

    added data in department table

--- index.js
+++ index.js
@@ -2,3 +2,17 @@
 const mysql = require('mysql2');
 
 const utils = require('util');
+
+//Connect to database
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //MySQL username,
+        user: 'root',
+        //MySQL password
+        password: 'alka@lucky16',
+        database: 'employeelist_db'
+
+    }
+)
