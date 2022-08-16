b5717706bbf77ce6b54314e5ef35e91090db40ff
Alka sah <alkasah485@gmail.com>
2022-08-16 01:38:24 -0700

    added tables in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -10,12 +10,25 @@ CREATE TABLE department (
     PRIMARY KEY (id) 
 );
 
-CREATE TABLE role(
+CREATE TABLE role1 (
     id INT ,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
+    PRIMARY KEY (id),
     FOREIGN KEY (department_id)
     REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+
+CREATE TABLE employee(
+    id INT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role1(id)
     ON DELETE SET NULL
 );
