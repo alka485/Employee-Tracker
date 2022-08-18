3601c663672cd161e402cf8fa029e71dac7b5b4b
Alka sah <alkasah485@gmail.com>
2022-08-18 01:28:02 -0700

    updation in index file

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 //Import and require mysql2
+const  inquirer = require('inquirer');
+//const choices = require('inquirer/lib/objects/choices');
 const mysql = require('mysql2');
 
 const utils = require('util');
@@ -18,3 +20,32 @@ const db = mysql.createConnection(
 )
 
 //select * from department;
+//select * from role;
+//select * from employee;
+
+const questions =() => {
+    return inquirer.prompt([
+        {
+            type : 'list',
+            name : 'details',
+            message: "What would you like to do?",
+            choices: ['View All departments',
+                       'View all roles',
+                       'View all employees',
+                       'Add a department',
+                       'Add a role',
+                       'Add an employee',
+                       'Update an employee role',
+                       'View employees by department',
+                       'Delete a department',
+                       'Delete a role',
+                       'Delete an employee',
+                       'No Action']
+                
+            
+        }
+    ]).then(response => {
+        const {choices} = response ;
+        
+    })
+}
