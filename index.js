97b3369a324a5a501a5650c2aaade34171a4b7af
Alka sah <alkasah485@gmail.com>
2022-08-21 00:47:56 -0700

    updation in index.js

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const  inquirer = require('inquirer');
 //const choices = require('inquirer/lib/objects/choices');
 const mysql = require('mysql2');
 
+
 const utils = require('util');
+
+//const cTable = require('console.table');
 
 //Connect to database
 
@@ -29,7 +32,7 @@ const questions =
     inquirer.prompt([
         {
             type : 'list',
-            name : 'details',
+            name : 'choice',
             message: "What would you like to do?",
             choices: ['View All departments',
                        'View all roles',
@@ -38,15 +41,32 @@ const questions =
                        'Add a role',
                        'Add an employee',
                        'Update an employee role',
-                       'View employees by department',
-                       'Delete a department',
-                       'Delete a role',
-                       'Delete an employee',
                        'No Action']
                 
             
         }
-    ]);
+    ]).then ((answers) => {
+        console.log(answers);
+        //const {choices} = answers;
+        console.log(answers.choice);
+        
+        if(answers.choice === 'View all employees'){
+            viewAllEmployees();
+        }
+    });
+    
+
+    // const viewAllEmployees =async () =>{
+    //     const emp = await db.query("SELECT id, first_name FROM employees");
+    //     console.table(emp);
+    //     const empChoices = emp.map(emp => ({
+    //         id : emp.id,
+    //         first_name : emp.first_name 
+
+
+    //     }));
+    //     console.log(empChoices);
+    // }
 
 // const viewDepartment = async () =>{
 //     const department = await db.query("Select * From department");
@@ -57,3 +77,23 @@ const questions =
 // console.log(department);
 
 // viewDepartment();
+
+//db.query= utils.promisify(db.query)
+
+const viewAllEmployees =() => {
+
+    //console.log("hello");
+
+
+     db.query("SELECT employee.id , employee.first_name,employee.last_name , role.title,role.salary,dpartment.name from employee ,role,department  WHERE department.id = role.department_id AND role.id = employee.role_id   ORDER BY employee.id ASC " ,
+                function(err,results){
+                    console.log(results);
+
+                }     )
+}
+
+// db.query('SELECT * FROM employee',
+//           function(err,results,fields){
+//             console.log(results);
+//             console.log(fields);
+//           } )
