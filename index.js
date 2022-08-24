3b08697cfc8eaa3385199b4aaf53f6fcda077844
Alka sah <alkasah485@gmail.com>
2022-08-24 01:13:24 -0700

    updation in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Import and require mysql2
 const { table } = require('console');
-const  inquirer = require('inquirer');
+const inquirer = require('inquirer');
 //const choices = require('inquirer/lib/objects/choices');
 const mysql = require('mysql2');
 const { type } = require('os');
@@ -19,64 +19,64 @@ const db = mysql.createConnection(
 )
 db.query = utils.promisify(db.query)
 
-   startPrompt();
-function startPrompt(){ 
-     inquirer.prompt([
+startPrompt();
+function startPrompt() {
+    inquirer.prompt([
         {
-            type : 'list',
-            name : 'choice',
+            type: 'list',
+            name: 'choice',
             message: "What would you like to do?",
             choices: ['View All Departments',
-                       'View All Roles',
-                       'View All Employees',
-                       'Add Department',
-                       'Add Role',
-                       'Add an employee',
-                       'Update an employee role',
-                       'No Action'],
-                 },       
+                'View All Roles',
+                'View All Employees',
+                'Add Department',
+                'Add Role',
+                'Add an employee',
+                'Update an employee role',
+                'No Action'],
+        },
     ])
-    .then(answers => {
-        const {choice} = answers;
-        console.log(choice);
-        switch (choice) {
-            case 'View All Departments':
-                 viewAllDepartment();
-                 break;
-            case 'View All Roles':
-                 viewAllRoles();
-                 break;
-            case 'View All Employees':
-                  viewAllEmployees();       
-                 break;
-            case 'Add Department':
-                 addDepartment();
-            case 'Add Role':
-                 addRole();          
-            default:
-                break;
-        }
-    });
+        .then(answers => {
+            const { choice } = answers;
+            console.log(choice);
+            switch (choice) {
+                case 'View All Departments':
+                    viewAllDepartment();
+                    break;
+                case 'View All Roles':
+                    viewAllRoles();
+                    break;
+                case 'View All Employees':
+                    viewAllEmployees();
+                    break;
+                case 'Add Department':
+                    addDepartment();
+                case 'Add Role':
+                    addRole();
+                default:
+                    break;
+            }
+        });
 }
-    
-const viewAllDepartment = async () =>{
+
+const viewAllDepartment = async () => {
     const department = await db.query("SELECT * FROM department");
     console.table(department);
     startPrompt();
-    }
+}
 
-const viewAllRoles = async () =>{
+const viewAllRoles = async () => {
     const role = await db.query(`SELECT role.id,
                                         role.title,
                                         department.name AS department,
                                         role.salary
                                         FROM role 
                                         JOIN department ON role.department_id=department.id`);
-    console.table(role);  
-    startPrompt();  
+    console.table(role);
+    startPrompt();
 }
 
-const viewAllEmployees =async () => {
+const viewAllEmployees = async () => {
     const emp = await db.query(`SELECT employee.id,
                                        employee.first_name,
                                        employee.last_name, 
@@ -97,67 +97,60 @@ const addDepartment = async () => {
         {
             message: "What is the name of the department?",
             name: "dept",
-            type:"input"
+            type: "input"
         }
     ]);
 
     await db.query(
-                 `INSERT INTO department(name) VALUES(?)`,
-                 [answers.dept]
-               
+        `INSERT INTO department(name) VALUES(?)`,
+        [answers.dept]
+
 
     );
-         console.log("Added "+answers.dept+" to the database");
+    console.log("Added " + answers.dept + " to the database");
 
-         startPrompt();
-    
+    startPrompt();
+
 }
 
 const addRole = async () => {
-
     const department = await db.query("SELECT * FROM department");
-    console.table(department);
-    const userChoices = department.map(user =>{
-        name: user.name
-    })
-
-   
-        if(err) throw err;
-    }
-
-
-    const answers = await inquirer.prompt([
+    //console.table(department);
+    const userChoices = department.map(name => {
+        name: department.name
+        value: department.id
+        //console.log(name);
+        
+        //console.log(userChoices.name);
+})
+console.log(userChoices);
+        const answers = await inquirer.prompt([
         {
             message: "What is the name of the role?",
             name: "role",
-            type:"input"
+            type: "input"
         },
         {
             message: "What is the salary of the role?",
             name: "sal",
-            type:"input"
+            type: "input"
         },
         {
             message: "Which department does the role belong to?",
             name: "dept",
-            type:"list",
-            choices :userChoices
+            type: "list",
+            choices: userChoices
         }
-
-
-    ]);
+     ]);
 
     await db.query(
-                 `INSERT INTO role(title,salary,department_id) VALUES(?,?,?)`,
-                 [answers.role,answers.salary,answers.dept]
-
-               
-
+        `INSERT INTO role(title,salary,department_id) VALUES(?,?,?)`,
+        [answers.role, answers.salary, answers.dept]
     );
-         //console.log("Added "+answers.dept+" to the database");
-         console.log("done");
-
-         startPrompt();
-    
+    //console.log("Added "+answers.dept+" to the database");
+    console.log("done");
 
 
+
+    startPrompt();
+    }
